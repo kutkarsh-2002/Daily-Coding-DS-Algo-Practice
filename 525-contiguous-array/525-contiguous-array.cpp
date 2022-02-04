0e7a43ebee38d71257318528668eebbd2366cf6f
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-02-04 16:44:13 +0530

    Time: 112 ms (88.54%), Space: 83.9 MB (25.19%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-    /* if(nums.size()==0 || nums.size()==1)
-         return 0;
-    if((nums[0]==0 && nums[1]==1) || (nums[0]==1 && nums[1]==0) && nums.size()==2)
-        return 2;
-        */
         
         
-    nums.insert(nums.begin(), 0);
-    
-    for(int i=1; i<nums.size(); i++){
-        if(nums[i]==0)
-            nums[i]=-1;
-    }
-    unordered_map<int, int>mp;
-    int length=0;
-    int sum=1;
-    mp[sum]=0;
-    for(int i=1; i<nums.size(); i++){
-        sum+=nums[i];
-        //cout<<sum<<endl<<endl;
-        if(mp.find(sum)!=mp.end()){
-            length=max(length, i-mp[sum]);
-        }else{
-            mp[sum]=i;
+        nums.insert(nums.begin(), 0);     //this will insert 0 in begining which will make vector 1 indexing.   
+        
+        //Unorder_map will help to store prefix sum as a key and index as value to analyze if same key repeats then there must be chance that combination of equal 1's & 0's.
+        
+        unordered_map<int, int>mp;
+        int length=0;
+        //prefix_sum(Generally prefix sum concepts help to solve range related problems)
+        
+        
+        int sum=1;
+        mp[sum]=0;
+        
+        for(int i=1; i<nums.size(); i++){
+            
+            //Replace all the 0 value with -1
+        //so that sum with equal combination of 1 & 0 result to 0.
+            nums[i]=(nums[i]==0)?-1:1;
+            sum+=nums[i];
+            
+            if(mp.find(sum)!=mp.end()){
+                //If sum repeats the there must be equal 1 & 0 in the range.
+                length=max(length, i-mp[sum]); // If range of length is greater than previous update the length value.
+            }else{
+                mp[sum]=i; //Insert sum as a key into map and index as a value when that sum is not present in map.
+            }
         }
-    }
-    
-    return length;
-        
+
+        return length;
+
     }
     
     
