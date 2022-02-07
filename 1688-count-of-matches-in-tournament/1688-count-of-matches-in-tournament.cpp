336c2d59eb818270ca4c305ab28062e5da30926f
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-02-07 10:50:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.33%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    int ans(int n, int countM){
+    /*int ans(int n, int countM){
         if(n==1){
-            return countM;
+            return countM; //if no. of team is equal to 1 then no matched will be played so simply return countM which is base case.
         }
         
         if(n%2==0){
-            countM+=n/2;
+            countM+=n/2; //count the number of matches in case number of team is even.
         }else{
-            countM+=(n-1)/2;
+            countM+=(n-1)/2; //count the number of matches in case number of team is odd.
         }
         
-     return (n%2==0)?ans(n/2, countM):ans((n-1)/2 +1, countM);
+     return (n%2==0)?ans(n/2, countM):ans((n-1)/2 +1, countM); //pass n/2 in function if no. of teams is even else pass (n-1)/2 +1 if no. of teams is odd.
     }
     int numberOfMatches(int n) {
         
         int countM=0;
         return ans(n, countM);
+    }*/
+    
+    int numberOfMatches(int n) {
+        
+       
+        return (n-1);
     }
+    
 };
