888987949066cc100c3c471c8b336899fa56bb1a
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-02-10 16:01:57 +0530

    Time: 19 ms (12.28%), Space: 11.2 MB (17.43%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        
+        
+        unordered_map<int, int>mp;
+        
+        for(int i=0; i<arr.size(); i++){
+            mp[arr[i]]++;
+        }
+        if(mp.find(0)!=mp.end() && mp[0]>1)
+            return true;
+        for(int i=0; i<arr.size(); i++){
+            if(mp.find(arr[i]*2)!=mp.end() && arr[i]*2!=0)
+                return true;
+        }
+        
+        return false;
+    }
+};
