0128e68e634a00018c2d5f8cf2074c19dcf6a661
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-02-10 13:34:59 +0530

    Time: 3 ms (72.10%), Space: 10.9 MB (70.83%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.cpp
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
         
-        unordered_map<int, int>mp;
+        /*unordered_map<int, int>mp;
         for(int i=0; i<nums.size(); i++){
             mp.insert({nums[i], i});
         }
@@ -16,6 +16,24 @@ public:
                 return -1;
         }
         
-        return mp[mx];
+        return mp[mx];*/
+        
+        int mx=nums[0];
+        int index=0;
+    
+        for(int i=1; i<nums.size(); i++){
+            if(mx<nums[i])
+            {
+                mx=nums[i];
+                index=i;
+            }
+        }
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[index]<2*nums[i] && index!=i)
+                return -1;
+        }
+        
+        return index;
     }
 };
