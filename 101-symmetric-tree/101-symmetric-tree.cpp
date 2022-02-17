aebb202a026045bb178552352f1164f450fd6f15
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-02-17 16:22:18 +0530

    Time: 8 ms (50.18%), Space: 16.4 MB (43.01%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>&v1){
+   /* void preorder(TreeNode* root, vector<int>&v1){
         if(!root)
         {
             v1.push_back(-101);
@@ -58,5 +58,24 @@ public:
             
             return true;
         }
+    }*/
+    
+    bool isMirror(TreeNode* root1, TreeNode* root2){
+	// If both trees are empty, then they are mirror images
+        if(!root1 && !root2)
+            return true;
+        
+        if(root1 && root2 && root1->val == root2->val)
+            return isMirror(root1->left, root2->right) && isMirror(root1->right, root2->left);
+			
+		// if none of above conditions is true then root1 and root2 are not mirror images
+        return false;
     }
+    
+	// Returns true if a tree is symmetric i.e. mirror image of itself
+    bool isSymmetric(TreeNode* root) {
+		// Check if tree is mirror of itself
+        return isMirror(root, root);
+    }
+    
 };
