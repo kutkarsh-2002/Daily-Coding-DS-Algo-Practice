54d2b74bed602f179f18010dbc394f1f5756fe90
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-03-01 15:20:48 +0530

    Time: 78 ms (58.98%), Space: 14.5 MB (87.22%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,28 +1,34 @@
+typedef pair<int, pair<int, int>> pi;
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         
-        vector<int> v={-100000,100000};
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        int mx=INT_MIN,n=nums.size(),d=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(nums[i][0]>mx)mx=nums[i][0];
-            pq.push({nums[i][0],i,0});
-        }        
-        while(!pq.empty()){
-            vector<int>mn=pq.top();
-            pq.pop();
-            int td=mx-mn[0];
-            if(td<d){
-                d=td;
-                v={mn[0],mx};
-            }
-            if(mn[2]+1<nums[mn[1]].size()){
-                if(nums[mn[1]][mn[2]+1]>mx)mx=nums[mn[1]][mn[2]+1];
-                pq.push({nums[mn[1]][mn[2]+1],mn[1],mn[2]+1});
-            }
-            else break;
+        int n = nums.size();
+        priority_queue<pi, vector<pi>, greater<pi>> pq;
+        int mn = INT_MAX, mx = INT_MIN, range = INT_MAX;
+        for (int i=0; i<n; i++) {
+            pq.push({nums[i][0], {i, 0}});
+            mn = min(mn, nums[i][0]);
+            mx = max(mx, nums[i][0]);
         }
-        return v;
+        int a = mn, b = mx;
+        while (!pq.empty()) {
+            pi curr = pq.top();
+            pq.pop();
+            //cout<<curr.first<<" "<<curr.second.first<<" "<<curr.second.second<<endl;
+            if (curr.second.second + 1 < nums[curr.second.first].size()) {
+                int r = curr.second.first, c = curr.second.second + 1;
+                pq.push({nums[r][c], {r, c}});
+                mn = pq.top().first;
+                mx = max(mx, nums[r][c]);
+                if (b-a > mx-mn) {
+                    a = mn;
+                    b = mx;
+                }
+            } else {
+                break;
+            }
+        }
+        return {a, b};
     }
 };
