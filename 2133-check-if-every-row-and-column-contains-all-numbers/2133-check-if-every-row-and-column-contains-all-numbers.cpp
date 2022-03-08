50442dd837b237e6b5d59cb984b3a78fe92d42c4
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-03-08 12:26:38 +0530

    Time: 554 ms (14.29%), Space: 67.8 MB (46.34%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    /* Idea of the problem
+    1.Use for loops to check each row for every number from 1 to n. Similarly, do the same for each column.
+    2.For each check, you can keep a set of the unique elements in the checked row/col. By the end of the check, the size of the set should be n.
+    */
+    
     bool checkValid(vector<vector<int>>& matrix) {
         unordered_set<int>st;
         int col=0, n=matrix.size();
-        for(int i=0; i<n; i++){
+        
+        for(int row=0; row<n; row++){
+            
+            //j is column traversing for each row
             
             for(int j=0; j<n; j++){
-                if(matrix[i][j]>=1 && matrix[i][j]<=n){
-                    st.insert(matrix[i][j]);
+                if(matrix[row][j]>=1 && matrix[row][j]<=n){
+                    st.insert(matrix[row][j]);
                 }
             }
             if(st.size()!=n)
@@ -15,9 +23,11 @@ public:
             
             st.clear();
             
-            for(int k=0; k<n; k++){
-                if(matrix[k][col]>=1 && matrix[k][col]<=n){
-                    st.insert(matrix[k][col]);
+            //i is row traversing for each col(column)
+            
+            for(int i=0; i<n; i++){
+                if(matrix[i][col]>=1 && matrix[i][col]<=n){
+                    st.insert(matrix[i][col]);
                 }
             }
             
@@ -25,7 +35,7 @@ public:
                 return false;
             
             st.clear();
-            col=i+1;
+            col=row+1;
         }
         
         return true;
