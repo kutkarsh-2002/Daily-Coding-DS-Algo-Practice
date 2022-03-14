9aa466db1fe68987e74d0ddb0097700b0fa092a7
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-03-15 00:00:57 +0530

    Time: 12 ms (31.79%), Space: 10.5 MB (31.93%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,84 +1,66 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        /*if(path.size()==1 && path[0]=='/'){
-            return "/";
+        
+    /*string res, tmp;
+    vector<string> stk;
+        
+    stringstream ss(path);
+        
+    while(getline(ss,tmp,'/')) {
+        if (tmp == "" or tmp == ".")
+            continue;
+        if (tmp == ".." and !stk.empty()) 
+            stk.pop_back();
+        else if (tmp != "..") 
+            stk.push_back(tmp);
+    }
+        
+    for(auto str : stk)
+        res += "/"+str;
+        
+    return res.empty() ? "/" : res;*/
+        
+        stack<string> st;
+        string res;
+        
+        for(int i = 0;  i<path.size(); ++i)
+        {
+            if(path[i] == '/')    
+                continue;
+            string temp;
+			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
+            while(i < path.size() && path[i] != '/')
+            {
+				// add path to temp string
+                temp += path[i];
+                ++i;
+            }
+            if(temp == ".")
+                continue;
+			// pop the top element from stack if exists
+            else if(temp == "..")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else
+			// push the directory file name to stack
+                st.push(temp);
         }
         
+		// adding all the stack elements to res
         
-        string ans="";
-        stack<char>st;
-        
-        for(int i=0; i<path.size(); i++){
-            
-            if(path[i]=='/' && st.empty()==true){
-                st.push('/');
-            }
-            else if(path[i]=='.')
-                continue;
-            else if(path[i]=='/' && !st.empty() && st.top()=='/'){
-                continue;
-            }
-            else{
-                st.push(path[i]);
-            }
-              
-        }
-        string temp="";
-        while(!st.empty()){
-            if(st.top()!='/'){
-                temp+=st.top();
-            }
-            else{
-                ans=ans+temp;
-                ans=ans+st.top();
-                temp="";
-            }
+        while(!st.empty())
+        {
+            res = "/" + st.top() + res;
             st.pop();
         }
         
+		// if no directory or file is present
+        if(res.size() == 0)
+            return "/";
         
-        reverse(ans.begin(), ans.end());
-        
-        if(ans.size()>1 && ans[ans.size()-1]=='/'){
-            ans.erase(ans.begin() + ans.size()-1);
-        }
-        
-        return ans;*/
-        
-        string res="";
-    int n = path.length();
-
-    
-    stack<string> stk;
-    
-    for(int i=0;i<n;i++) {
-        
-        if(path[i]=='/')
-            continue;
-        
-        string tmpPath = "";
-        
-        while(i<n && path[i]!='/')
-            tmpPath+=path[i++];
-        
-        if(tmpPath==".")
-            continue;
-        
-        else if(tmpPath==".."){ // going one directory up.
-            if(!stk.empty())
-                stk.pop();
-        }
-        else // also taking care of this care when (/..someFileName or /...someFileName) this is a valid file.
-            stk.push(tmpPath);
-    }
-        
-    string ans="";
-    while(!stk.empty()){
-        ans="/"+stk.top()+ans; // inserting in the begining.
-        stk.pop();
-    }
-    
-    return (ans==""?"/":ans); // when the stack is empty in this case ( /////// or /../ )
+        return res;
     }
 };
