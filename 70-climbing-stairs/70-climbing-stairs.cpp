c8672730e6cae85139c9e0c75c1a8b5f3f64d963
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-03-28 23:48:05 +0530

    Time: 5 ms (10.37%), Space: 6.2 MB (29.80%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    int fib(int n, vector<int>&dp){
+        
+        if(n==0){
+            return 1;
+        }
+        
+        if(n<0){
+            return 0;
+        }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        return dp[n]=fib(n-2, dp)+fib(n-1, dp);
+        
+    }
+    
+    int climbStairs(int n) {
+        
+        vector<int>dp(n+1, -1);
+        
+        return fib(n, dp);
+    }
+};
