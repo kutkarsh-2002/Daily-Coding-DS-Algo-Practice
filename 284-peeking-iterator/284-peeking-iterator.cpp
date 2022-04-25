a9ceabf6e29247eb58e307c27f0f6dee6b0616f2
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-04-25 22:41:03 +0530

    Time: 6 ms (31.17%), Space: 7.3 MB (88.55%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.cpp
@@ -0,0 +1,83 @@
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+/*class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    
+	}
+	
+	bool hasNext() const {
+	    
+	}
+};*/
+
+class PeekingIterator : public Iterator {
+    
+    int nextVal; 
+    
+    bool iterHasNext; 
+    
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+        
+        iterHasNext=Iterator::hasNext();
+        
+	    if (iterHasNext) nextVal = Iterator::next(); 
+        
+	}
+	
+	int peek() {
+        
+        return nextVal;
+        
+    }
+	
+	int next() {
+        
+	    int currNext = nextVal; 
+        
+        iterHasNext=Iterator::hasNext(); 
+        
+	    if (iterHasNext) nextVal = Iterator::next(); 
+        
+	    return currNext; 
+    }
+	
+	bool hasNext() const {
+        
+	    return iterHasNext; 
+        
+	}
+    
+};
