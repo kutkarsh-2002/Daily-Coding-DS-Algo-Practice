c00d7784069bcc761a0694c6f7cd33bea04d215e
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-05-13 13:15:28 +0530

    Time: 61 ms (20.23%), Space: 16.2 MB (9.91%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        /*stack<pair<char, int>>st;
+        int n=s.size();
+        
+        for(int i=0; i<n; i++){
+            if(st.empty()){
+                st.push({s[i], 1});
+            }
+            else if(!st.empty() && st.top().first!=s[i]){
+                if(st.top().second>1){
+                    st.pop();
+                }
+                
+                if(!st.empty() && st.top().first==s[i]){
+                    auto p=st.top();
+                    st.pop();
+                    st.push({s[i], p.second+1});
+                }else{
+                    st.push({s[i], 1});
+                }
+            }
+            else{
+                
+                auto p=st.top();
+                st.pop();
+                st.push({s[i], p.second+1});
+            }
+        }
+        
+        string ans="";
+        
+        while(!st.empty()){
+            
+            if(st.top().second>1){
+                st.pop();
+            }
+            else{
+                ans.push_back(st.top().first);
+                st.pop();
+            }
+            
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;*/
+        
+        stack<pair<char, int>>st;
+        
+        int n=s.size();
+        
+        if(n<2)
+            return s;
+        
+        for(int i=0; i<n; i++){
+            if(st.empty() || st.top().first!=s[i])
+                st.push({s[i], 1});
+            else{
+                auto p=st.top();
+                st.pop();
+                st.push({s[i], p.second+1});
+            }
+            
+            if(st.top().second==2)
+                st.pop();
+            
+        }
+        
+        
+        string ans="";
+        
+        while(!st.empty()){
+            auto p=st.top();
+            st.pop();
+            
+            while(p.second--){
+                ans.push_back(p.first);
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
