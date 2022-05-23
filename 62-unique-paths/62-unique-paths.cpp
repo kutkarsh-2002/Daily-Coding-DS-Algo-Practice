cd429b465a518684c1652e7bc5fbcdc291c0ff03
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-05-23 19:33:31 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.56%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int TotalUpaths(int i, int j, int &m, int &n,vector<vector<int>>&dp){
+    int TotalUpaths(int i, int j, vector<vector<int>>&dp){
         if(i==0 && j==0){
             return 1;
         }
@@ -9,8 +9,8 @@ public:
         
         int l, t;
         
-        t=(i-1<0)?0:TotalUpaths(i-1, j, m, n, dp);
-        l=(j-1<0)?0:TotalUpaths(i, j-1, m, n, dp);
+        t=(i-1<0)?0:TotalUpaths(i-1, j, dp);
+        l=(j-1<0)?0:TotalUpaths(i, j-1, dp);
         
         
         return dp[i][j]=l+t;
@@ -36,6 +36,6 @@ public:
 //         return dp[m-1][n-1];
         
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        return TotalUpaths(m-1 ,n-1, m, n, dp);
+        return TotalUpaths(m-1 ,n-1, dp);
     }
 };
