e394a38d5cd1fae30c385c724fcb4a8634bc2449
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-05-31 00:13:52 +0530

    Time: 24 ms (38.92%), Space: 7.2 MB (84.37%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int n=word1.size(), m=word2.size();
+//     int minedit(int i, int j, string& s, string& t, vector<vector<int>>dp){
+//     if(i<0) return j+1;
+//     if(j<0) return i+1;
+    
+//     if(dp[i][j]!=-1) return dp[i][j];
+    
+//     if(s[i]==t[j])
+//         return dp[i][j]=0+minedit(i-1, j-1, s, t, dp);
+//     else
+//         return dp[i][j]=1+min(minedit(i-1, j-1, s, t, dp), min(minedit(i, j-1, s, t, dp), minedit(i-1, j, s, t, dp)));
+// } 
+
+    int minDistance(string s, string t) {
+        int n=s.size(), m=t.size();
         
         int dp[n+1][m+1];
         memset(dp, 0, sizeof(dp));
@@ -18,15 +30,22 @@ public:
                 else if(j==0){
                     dp[i][j]=i;
                 }
-                else if(word1[i-1]==word2[j-1]){
+                else if(s[i-1]==t[j-1]){
+                    //if both character are same then no operation will be required.
                     dp[i][j]=dp[i-1][j-1];
                 }
                 else{
+                    // if both character is diffrenet try to find the min operation from all remove, insert, and delete
                     dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j], dp[i][j-1]));
                 }    
             }
         }
         
         return dp[n][m];
+        
+        // int n=s.size();
+        // int m=t.size();
+        // vector<vector<int>>dp(n, vector<int>(m, -1));
+        // return minedit(n-1, m-1, s, t, dp);
     }
 };
