2a96b1bef25462e05dc2af26fa3ffeaa150c95dd
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-01 11:40:45 +0530

    Time: 148 ms (59.63%), Space: 55 MB (89.59%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,10 +1,39 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int n=prices.size();
-        vector<vector<int>>dp(n+1, vector<int>(2, -1));
+//         int n=prices.size();
+//         vector<vector<int>>dp(n+1, vector<int>(2, -1));
         
-        dp[n][0]=0, dp[n][1]=0;
+//         dp[n][0]=0, dp[n][1]=0;
+        
+//         for(int i=n-1; i>=0; i--){
+            
+//             int profit=0;
+            
+//             for(int buy=0; buy<=1; buy++){
+//                 if(buy){
+        
+//                     profit=max(-prices[i]+dp[i+1][0], dp[i+1][1]);
+//                  }
+//                 else{
+        
+//                     profit=max(prices[i]-fee+dp[i+1][1], dp[i+1][0]);
+//             }
+                
+//             dp[i][buy]=profit;
+                
+//             }
+          
+//         }
+        
+        
+//         return dp[0][1];
+        
+        /* Space Optimization */
+        
+        int n=prices.size();
+        vector<int>ahead(2, 0);
+        ahead[0]=0, ahead[1]=0;
         
         for(int i=n-1; i>=0; i--){
             
@@ -13,20 +42,21 @@ public:
             for(int buy=0; buy<=1; buy++){
                 if(buy){
         
-                    profit=max(-prices[i]+dp[i+1][0], dp[i+1][1]);
+                    profit=max(-prices[i]+ahead[0], ahead[1]);
                  }
                 else{
         
-                    profit=max(prices[i]-fee+dp[i+1][1], dp[i+1][0]);
+                    profit=max(prices[i]-fee+ahead[1], ahead[0]);
             }
                 
-            dp[i][buy]=profit;
+            ahead[buy]=profit;
                 
             }
           
         }
         
         
-        return dp[0][1];
-    }
+        return ahead[1];
+        
+   }
 };
