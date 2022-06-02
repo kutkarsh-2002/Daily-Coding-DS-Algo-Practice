7d4a00542ddd646fb29be9d780beebcadefbfbdc
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-02 15:18:24 +0530

    Time: 14 ms (77.79%), Space: 10.5 MB (65.49%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -25,17 +25,70 @@ public:
         /* Tabulation */
         
        
-        vector<int>dp(n, 1);
+//         vector<int>dp(n, 1);
         
-        for(int i=0; i<n; i++){
-            for(int prev=0; prev<i; prev++){
+//         for(int i=0; i<n; i++){
+//             for(int prev=0; prev<i; prev++){
                 
-                if(nums[i]>nums[prev]){
-                    dp[i]=max(dp[i], 1+dp[prev]);
-                }
+//                 if(nums[i]>nums[prev]){
+//                     dp[i]=max(dp[i], 1+dp[prev]);
+//                 }
+//             }
+//         }
+        
+//         return *max_element(dp.begin(), dp.end());
+        
+    /* Printing LIS */
+        
+//         vector<int>dp(n, 1), hash(n);
+//         int maxi=1;
+//         int lastInd=0;
+        
+//         for(int i=0; i<n; i++){
+//             hash[i]=i;
+//             for(int prev=0; prev<i; prev++){
+                
+//                 if(nums[i]>nums[prev] && dp[i]<1+dp[prev]){
+//                     dp[i]=1+dp[prev];
+//                     hash[i]=prev;
+//                 }
+//             }
+//             if(dp[i]>maxi){
+//                 maxi=dp[i];
+//                 lastInd=i; 
+//             }
+//         }
+//         vector<int>temp;
+        
+//         while(hash[lastInd]!=lastInd){
+//             temp.push_back(nums[lastInd]);
+//             lastInd=hash[lastInd];
+//         }
+//         temp.push_back(nums[lastInd]);
+//         reverse(temp.begin(), temp.end());
+        
+//         for(auto x: temp){
+//             cout<<x<<" ";
+//         }
+        
+//         return maxi;
+        
+        /* Binary Search */
+        
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                
+                int ind=lower_bound(temp.begin(), temp.end(), nums[i])-temp.begin();
+                temp[ind]=nums[i];
             }
         }
         
-        return *max_element(dp.begin(), dp.end());
+        return temp.size();
     }
 };
