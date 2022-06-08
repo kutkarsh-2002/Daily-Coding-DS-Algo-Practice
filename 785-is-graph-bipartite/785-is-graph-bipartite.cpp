582d784605e5210099924498c2f99cb2a364cfd5
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-08 21:04:08 +0530

    Time: 23 ms (92.27%), Space: 13.5 MB (54.62%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    bool bfsBip(int u, vector<int>&colr, vector<vector<int>>& graph){
-        queue<int>q;
-        q.push(u);
-        colr[u]=0;
+//     bool bfsBip(int u, vector<int>&colr, vector<vector<int>>& graph){
+//         queue<int>q;
+//         q.push(u);
+//         colr[u]=0;
         
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
+//         while(!q.empty()){
+//             int node=q.front();
+//             q.pop();
             
-            for(auto x:graph[node]){
-                if(colr[x]==-1){
-                    q.push(x);
-                    colr[x]=!colr[node];
-                }
-                else if(colr[x]!=-1 && colr[x]==colr[node]) return false;
+//             for(auto x:graph[node]){
+//                 if(colr[x]==-1){
+//                     q.push(x);
+//                     colr[x]=!colr[node];
+//                 }
+//                 else if(colr[x]!=-1 && colr[x]==colr[node]) return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+    
+    bool dfs(int u, int p, vector<int>&colr, vector<vector<int>>& graph){
+        
+        if(p==-1){
+            colr[u]=0;
+        }else{
+            colr[u]=!colr[p];
+        }
+        
+        for(auto x:graph[u]){
+            if(colr[x]==-1){
+                if(dfs(x, u, colr, graph)==false) return false;
             }
+            else if(colr[x]!=-1 && colr[x]==colr[u]) return false;
         }
         
         return true;
@@ -27,7 +46,7 @@ public:
         
         for(int i=0; i<n; i++){
             if(colr[i]==-1){
-                if(bfsBip(i, colr, graph)==false) return false;
+                if(dfs(i, -1, colr, graph)==false) return false;
             }
         }
         
