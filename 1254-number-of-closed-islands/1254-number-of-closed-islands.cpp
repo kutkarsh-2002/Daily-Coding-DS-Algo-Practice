2c75b872b405b5ccaa39cd5e8ffb26af7f610fa1
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-17 18:28:33 +0530

    Time: 13 ms (83.40%), Space: 9.5 MB (66.18%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<int>>&grid){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==1)
+            return ;
+        
+        //vis[i][j]=1;
+        grid[i][j]=1;
+        
+        dfs(i-1, j, grid);
+        dfs(i, j-1, grid);
+        dfs(i, j+1, grid);
+        dfs(i+1, j, grid);
+    }
+    
+    int closedIsland(vector<vector<int>>& grid) {
+        //vector<vector<int>>vis(grid.size(), vector<int>(grid[0].size(), 0));
+        int count=0;
+        
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j]==0 && (i*j==0 || (i==grid.size()-1) || (j==grid[0].size()-1))){
+                    dfs(i, j, grid); 
+                }
+            }
+        }
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j]==0){
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
