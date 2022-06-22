604fe370167658a31f9d8da54bf58afda49ef76a
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-22 12:18:18 +0530

    Time: 448 ms (44.74%), Space: 88.1 MB (39.93%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,20 +1,60 @@
 class Solution {
 public:
     
+//     struct comparator{
+        
+//     bool operator()(string &s1, string&s2){
+//         if(s1.length()>s2.length())
+//             return true;
+//         else if(s1.length()<s2.length())
+//             return false;
+        
+//         else{
+            
+//             for(int i=0; i<s1.size(); i++){
+//                 if(s1[i]-'0'>s2[i]-'0')
+//                     return true;
+//                 else if(s1[i]-'0'<s2[i]-'0')
+//                     return false;
+//             }
+//         }
+        
+//         return false;
+//     }
+// };
+    
+//     string kthLargestNumber(vector<string>& nums, int k) {
+        
+//         //min heap
+//         priority_queue<string, vector<string>, comparator>pq;
+        
+//         for(auto it:nums){
+//             pq.push(it);
+//             //cout<<it<<" "<<endl;
+            
+//             if(pq.size()>k){
+//                 pq.pop();
+//             }
+//         }
+        
+//         return pq.top();
+        
+//     }
+    
     struct comparator{
         
     bool operator()(string &s1, string&s2){
-        if(s1.length()>s2.length())
+        if(s1.length()<s2.length())
             return true;
-        else if(s1.length()<s2.length())
+        else if(s1.length()>s2.length())
             return false;
         
         else{
             
             for(int i=0; i<s1.size(); i++){
-                if(s1[i]-'0'>s2[i]-'0')
+                if(s1[i]-'0'<s2[i]-'0')
                     return true;
-                else if(s1[i]-'0'<s2[i]-'0')
+                else if(s1[i]-'0'>s2[i]-'0')
                     return false;
             }
         }
@@ -28,16 +68,20 @@ public:
         //min heap
         priority_queue<string, vector<string>, comparator>pq;
         
-        for(auto it:nums){
-            pq.push(it);
-            //cout<<it<<" "<<endl;
-            
-            if(pq.size()>k){
-                pq.pop();
-            }
+        for(auto x:nums){
+            pq.push(x);
         }
         
-        return pq.top();
+        string ans="";
+        
+        while(!pq.empty() && k>0){
+            ans=pq.top();
+            //cout<<pq.top()<<endl;
+            pq.pop();
+            k--;
+        }
+        
+        return ans;
         
     }
 };
