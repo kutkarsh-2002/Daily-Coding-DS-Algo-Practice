56ec9c25d727b83d5e6ff504a7c7d72b06ca74ee
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-24 22:02:56 +0530

    Time: 78 ms (37.18%), Space: 41.4 MB (42.05%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -12,34 +12,49 @@
 class Solution {
 public:
     
-    void dfs(TreeNode* root, int& sum){
-        if(root==NULL)
-            return ;
+//     void dfs(TreeNode* root, int& sum){
+//         if(root==NULL)
+//             return ;
         
-        if(root->val%2==0){
-            if(root->left!=NULL && root->left->left!=NULL)
-                sum+=root->left->left->val;
-            if(root->left!=NULL && root->left->right!=NULL)
-                sum+=root->left->right->val;
+//         if(root->val%2==0){
+//             if(root->left!=NULL && root->left->left!=NULL)
+//                 sum+=root->left->left->val;
+//             if(root->left!=NULL && root->left->right!=NULL)
+//                 sum+=root->left->right->val;
             
-            if(root->right!=NULL && root->right->left!=NULL)
-                sum+=root->right->left->val;
-            if(root->right!=NULL && root->right->right!=NULL)
-                sum+=root->right->right->val;
+//             if(root->right!=NULL && root->right->left!=NULL)
+//                 sum+=root->right->left->val;
+//             if(root->right!=NULL && root->right->right!=NULL)
+//                 sum+=root->right->right->val;
             
-        }
+//         }
         
-        dfs(root->left, sum);
-        dfs(root->right, sum);
+//         dfs(root->left, sum);
+//         dfs(root->right, sum);
         
         
-    }
+//     }
     
-    int sumEvenGrandparent(TreeNode* root) {
-        int sum=0;
+//     int sumEvenGrandparent(TreeNode* root) {
+//         int sum=0;
         
-        dfs(root, sum);
+//         dfs(root, sum);
         
-        return sum;
+//         return sum;
+//     }
+    
+    int s=0;
+    void find(TreeNode *gp,TreeNode *p, TreeNode * child)
+    {
+        if(child==NULL) return; 
+        if(gp!=NULL&&gp->val%2==0){ s+=child->val;} 
+        //make parent to gp, child to parent ,child= child->child 
+        find(p,child,child->left);
+        find(p,child,child->right);
+    }
+    int sumEvenGrandparent(TreeNode* root) { 
+        
+        find(NULL,NULL,root);  //we are passing grandparent(gp),parent(p),child
+        return s;
     }
 };
