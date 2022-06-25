8c43253ca5ddbbbe1373860fe0942f4150dea171
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-06-25 12:34:59 +0530

    Time: 791 ms (5.01%), Space: 73.4 MB (11.88%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,5 +1,7 @@
+ #define pii pair<int, pair<int, int>>
 class Solution {
 public:
+   
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int res = 1;
         int row = grid.size();
@@ -8,24 +10,29 @@ public:
         if (col == 0 ) return -1;
         if (grid[0][0] != 0 | grid[row-1][col-1] != 0) return -1;
 
-        queue<pair<int, int>> queue;
-        queue.push(make_pair(0,0));
+        //queue<pair<int, int>> queue;
+        priority_queue<pii, vector<pii>, greater<pii>>pq; 
+        //queue.push(make_pair(0,0));
+        
+        pq.push({1, {0, 0}});
         vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
         grid[0][0] = 1;
-        while(!queue.empty()){
-            auto curr = queue.front();
-            int x = curr.first, y = curr.second;
+        while(!pq.empty()){
+            //auto curr = queue.front();
+            auto curr=pq.top();
+            int x = curr.second.first, y = curr.second.second, dis=curr.first;
             if( x == row -1 && y == col -1) return grid[x][y];
 
             for(auto direction : directions){
                 int nx = x + direction[0];
                 int ny = y + direction[1];
                 if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                    queue.push(make_pair(nx,ny));
-                    grid[nx][ny] = grid[x][y] + 1;
+                    //queue.push(make_pair(nx,ny));
+                    pq.push({dis+1, {nx, ny}});
+                    grid[nx][ny] = dis + 1;
                 }
             }
-            queue.pop();
+            pq.pop();
         }
         return -1;
     }
