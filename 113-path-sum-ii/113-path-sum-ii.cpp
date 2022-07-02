93327b54c6da4c5f16445b21117e3aeb1bd26af8
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-07-02 15:54:11 +0530

    Time: 6 ms (95.61%), Space: 20 MB (49.15%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,25 +12,47 @@
 class Solution {
 public:
     
-    void getAllpaths(TreeNode* root, vector<int>temp, vector<vector<int>>&mat, int targetSum){
-        if(root==NULL){
-            return ;
-        }
+//     void getAllpaths(TreeNode* root, vector<int>temp, vector<vector<int>>&mat, int targetSum){
+//         if(root==NULL){
+//             return ;
+//         }
         
-        if(root->left==NULL && root->right==NULL && root->val==targetSum){
-            temp.push_back(root->val);
-            mat.push_back(temp);
-            return ;
+//         if(root->left==NULL && root->right==NULL && root->val==targetSum){
+//             temp.push_back(root->val);
+//             mat.push_back(temp);
+//             return ;
             
+//         }
+        
+//         temp.push_back(root->val);
+//         getAllpaths(root->left, temp, mat, targetSum-root->val);
+//         getAllpaths(root->right, temp, mat, targetSum-root->val);
+//     }
+//     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+//         vector<vector<int>>mat;
+//         getAllpaths(root, {}, mat,targetSum);
+//         return mat;
+        
+//     }
+    
+    
+    void pathStore(TreeNode* root, vector<int>& path, int targetSum, vector<vector<int>>& res){
+        if(root == NULL)
+            return;	
+        path.push_back(root->val);
+        if(root->right == NULL and root->left == NULL and targetSum == root->val){
+            res.push_back(path);
+            path.pop_back();
+            return;
         }
-        temp.push_back(root->val);
-        getAllpaths(root->left, temp, mat, targetSum-root->val);
-        getAllpaths(root->right, temp, mat, targetSum-root->val);
+        pathStore(root->left, path, targetSum - root->val, res);
+        pathStore(root->right, path, targetSum - root->val, res); 
+        path.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>>mat;
-        getAllpaths(root, {}, mat,targetSum);
-        return mat;
-        
+        vector<int> path;
+        vector<vector<int>> res;
+        pathStore(root, path, targetSum, res);
+        return res;
     }
 };
