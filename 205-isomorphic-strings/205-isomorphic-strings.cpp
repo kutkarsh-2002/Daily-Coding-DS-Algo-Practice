c700376e219872d4c0a2e142ed50ce89debad8db
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-07-04 23:25:12 +0530

    Time: 18 ms (19.02%), Space: 7 MB (75.23%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -4,59 +4,36 @@ public:
         if(s.size()!=t.size())
             return false;
         
-        //string x=s, y=s;
         unordered_map<char, char>mp;
-        
-        
-        
+               
         
         for(int i=0; i<s.size(); i++){
             if(mp.find(t[i])!=mp.end() && mp[t[i]]!=s[i]){
                 return false;
             }
-//             else if((mp.find(t[i])!=mp.end() && mp[t[i]]==s[i])){
-//                 string temp="";
-//                 temp+=t[i];
-//                 y.replace(i, 1, temp);
-                
-//             }
             else if(mp.empty() || mp.find(t[i])==mp.end()){
-                // string temp="";
-                // temp+=t[i];
+                
                 mp.insert({t[i], s[i]});
-                //y.replace(i, 1, temp);
+                
                 
                 
             }
         }
         
-        // if(y!=t) return false;
+        
         mp.clear();
         
         for(int i=0; i<s.size(); i++){
             if(mp.find(s[i])!=mp.end() && mp[s[i]]!=t[i]){
                 return false;
             }
-//             else if((mp.find(s[i])!=mp.end() && mp[s[i]]==t[i])){
-//                 string temp="";
-//                 temp+=t[i];
-//                 x.replace(i, 1, temp);
-                
-                
-//             }
             else if(mp.empty() || mp.find(s[i])==mp.end()){
-                // string temp="";
-                // temp+=t[i];
+                
                 mp.insert({s[i], t[i]});
-                //x.replace(i, 1, temp);
-                
-                
-                
+                  
             }
         }
         
-        
-        // if(x!=t) return false;
         
         return true;
         
