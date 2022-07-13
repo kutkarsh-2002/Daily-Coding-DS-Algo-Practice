aeac374b4bb44d66f2c9331eb0e79c3a26f3c4d8
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-07-13 16:35:08 +0530

    Added solution - LeetHub

--- Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.cpp
+++ Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.cpp
@@ -93,32 +93,13 @@ struct Node {
 class Solution{
   public:
     // Return True if the given trees are isomotphic. Else return False.
-    bool isSame(Node *root1, Node *root2)
-    {   if(root1==NULL && root2==NULL) return true;
-        if((root1==NULL && root2!=NULL) || (root1!=NULL && root2==NULL)) return false;
-        if(root1->data!=root2->data) return false;
-        
-        return isSame(root1->left, root2->left) && isSame(root1->right, root2->right);
-        
-    }
-    
-    bool onFlip(Node *root1, Node *root2){
-        if(root1==NULL && root2==NULL) return true;
-        if((root1==NULL && root2!=NULL) || (root1!=NULL && root2==NULL)) return false;
-        if(root1->data!=root2->data) return false;
-        
-        return onFlip(root1->left, root2->right) && onFlip(root1->right, root2->left);
-    }
     
     bool isIsomorphic(Node *root1, Node *root2)
     {   if(root1==NULL && root2==NULL) return true;
+        if((root1==NULL && root2!=NULL) || (root1!=NULL && root2==NULL)) return false;
+        if(root1->data!=root2->data) return false;
         
-        if(isSame(root1, root2)) return true;
-        else{
-            return onFlip(root1, root2);
-        }
-        
-        
+        return (isIsomorphic(root1->left, root2->left) && isIsomorphic(root1->right, root2->right))||(isIsomorphic(root1->left, root2->right) && isIsomorphic(root1->right, root2->left));
         
     }
 };
