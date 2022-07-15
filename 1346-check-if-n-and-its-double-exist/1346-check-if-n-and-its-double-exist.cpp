6adbe02b18e0324e572467e4de343386c208636c
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-07-15 16:50:04 +0530

    Time: 28 ms (5.17%), Space: 11.2 MB (15.96%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -8,8 +8,10 @@ public:
         for(int i=0; i<arr.size(); i++){
             mp[arr[i]]++;
         }
+        
         if(mp.find(0)!=mp.end() && mp[0]>1)
             return true;
+        
         for(int i=0; i<arr.size(); i++){
             if(mp.find(arr[i]*2)!=mp.end() && arr[i]*2!=0)
                 return true;
