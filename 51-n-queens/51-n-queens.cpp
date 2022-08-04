40b11dc7c497a0616822f0f647de855f3668aef3
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-08-04 11:44:56 +0530

    Time: 6 ms (82.27%), Space: 7.1 MB (87.78%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -16,37 +16,91 @@ public:
     
     /***********************************************************/
     
-    bool isValid(int row, int col, int n, vector<string>&board){
-        int dprow=row, dpcol=col;
+//     bool isValid(int row, int col, int n, vector<string>&board){
+//         int dprow=row, dpcol=col;
         
-        //upper diag
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
+//         //upper diag
+//         while(row>=0 && col>=0){
+//             if(board[row][col]=='Q') return false;
+//             row--;
+//             col--;
+//         }
         
-        row=dprow, col=dpcol;
+//         row=dprow, col=dpcol;
         
-        //lower diag
-        while(row<n && col>=0){
-            if(board[row][col]=='Q') return false;
-            row++;
-            col--;
-        }
+//         //lower diag
+//         while(row<n && col>=0){
+//             if(board[row][col]=='Q') return false;
+//             row++;
+//             col--;
+//         }
         
-        //left row
-        col=dpcol;
-        row=dprow;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
+//         //left row
+//         col=dpcol;
+//         row=dprow;
+//         while(col>=0){
+//             if(board[row][col]=='Q') return false;
+//             col--;
+//         }
         
-        return true;
-    }
+//         return true;
+//     }
     
-    void dfs(int col, int n, vector<string>&board, vector<vector<string>>&res){
+//     void dfs(int col, int n, vector<string>&board, vector<vector<string>>&res){
+//         if(col==n){
+//             //if col==n i.e we have successfully placed Q at the right position
+//             // so we will push board into result.
+//             res.push_back(board);
+//             return ;
+//         }
+        
+//         for(int row=0; row<n; row++){
+//             // for every col we will check into each row that is it posiible to place Q until we get the right pos.
+            
+//             if(isValid(row, col, n, board)){
+//                 board[row][col]='Q';
+//                 dfs(col+1, n, board, res);
+//                 board[row][col]='.';
+//             }
+//         }
+//     }
+    
+//     vector<vector<string>> solveNQueens(int n) {
+        
+//         if(n==0) return {{}};
+//         vector<vector<string>>res;
+//         vector<string>board(n);
+//         string s(n, '.');
+        
+//         for(int i=0; i<n; i++){
+//             board[i]=s;
+//         }
+        
+//         dfs(0, n, board, res);
+//         return res;
+//     }
+    
+    
+    /***********************************************/
+    
+//     In order to optimize the code we need to use the concept of hashing.
+//     We will create 3 vector in order to have a track of cell containing Q.
+//     1. updiag -> size-> 2*n-1
+//     2. lowdiag -> size -> 2*n-1
+//     3. left ->   size ->  n
+        
+//     Note: In order to fill the various vector array use this formula.
+        
+//         updiag[n-1+col-row]=1
+//         lowdiag[row+col]=1
+//         lrow[row]=1
+        
+    
+    
+    /*********************************************/
+    
+       
+    void dfs(int col, int n, vector<string>&board, vector<int>&updiag, vector<int>&lowdiag, vector<int>&lrow, vector<vector<string>>&res){
         if(col==n){
             //if col==n i.e we have successfully placed Q at the right position
             // so we will push board into result.
@@ -57,15 +111,21 @@ public:
         for(int row=0; row<n; row++){
             // for every col we will check into each row that is it posiible to place Q until we get the right pos.
             
-            if(isValid(row, col, n, board)){
+            if(lrow[row]==0 && updiag[n-1+col-row]==0 && lowdiag[row+col]==0){
                 board[row][col]='Q';
-                dfs(col+1, n, board, res);
+                updiag[n-1+col-row]=1;
+                lowdiag[row+col]=1;
+                lrow[row]=1;
+                dfs(col+1, n, board, updiag, lowdiag, lrow, res);
                 board[row][col]='.';
+                updiag[n-1+col-row]=0;
+                lowdiag[row+col]=0;
+                lrow[row]=0;
             }
+            
         }
     }
-    
-    vector<vector<string>> solveNQueens(int n) {
+       vector<vector<string>> solveNQueens(int n) {
         
         if(n==0) return {{}};
         vector<vector<string>>res;
@@ -76,7 +136,8 @@ public:
             board[i]=s;
         }
         
-        dfs(0, n, board, res);
+        vector<int>updiag(2*n-1, 0), lowdiag(2*n-1, 0), lrow(n, 0);
+        dfs(0, n, board, updiag, lowdiag, lrow, res);
         return res;
     }
 };
