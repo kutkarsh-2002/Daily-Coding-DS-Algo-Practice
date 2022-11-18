490fcc610baa4d5a9911341020813d3a494c9cb8
kumar utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-11-18 19:30:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    /*bool isUgly(int n) {
+        if(n<14){
+            return true;
+        }
+        if(n<0){
+            n=(-n);
+        }
+        for(int i=2; i<=n/2; i++){
+            if((n%(6*i-1)==0 || n%(6*i+1)==0) || n%7==0){
+                return false;
+            }
+        }
+        return true;
+    }*/
+    
+    bool isUgly(int n) {
+        while(n!=1){
+            int temp = n;
+            if(n%2 == 0){
+                n/=2;
+            }
+            
+            if(n%3 == 0){
+                n/=3;
+            }
+            
+            if(n%5 == 0){
+                n/=5;
+            }
+            
+            if(temp == n){
+                return 0;
+            }
+    
+        }
+        
+        return 1;
+    }
+};
