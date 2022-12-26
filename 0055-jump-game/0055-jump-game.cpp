3ae43bbbb57fa131235a973b710a3af85aa1c9f0
Utkarsh <71220973+kutkarsh-2002@users.noreply.github.com>
2022-12-26 21:38:35 +0530

    Time: 77 ms (82.29%), Space: 48.5 MB (48.80%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /*bool check(vector<int>&nums, int st){
+        if(st>=nums.size()-1)
+            return true;
+        else if(nums[st]==0 && st<nums.size()-1){
+            return false;
+        }
+        
+        for(int i=0; i<nums[st] && st<nums.size();){
+            check(nums, i+1);
+            //check(nums, i-1);
+        }
+        
+        return false;
+    }*/
+    //Capture the maximum projectile distance it has reached
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size();
+        int reachable=0;
+        
+        for(int i=0; i<nums.size(); i++){
+            
+            if(reachable<i)
+                //we can't jump from that index
+                return false;
+            
+            reachable=max(reachable, i+nums[i]);
+        }
+        
+        return true;
+    }
+};
